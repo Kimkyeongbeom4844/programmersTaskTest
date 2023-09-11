f5a8c3c6b69c7942c22df9a5af8bc7b4d7be4079
kimKyeongbeom <bibix321@naver.com>
2023-09-11 14:28:22 +0900

    이벤트 중복 메모리 누수 오류 수정

--- 사원정보테이블구축/index.js
+++ 사원정보테이블구축/index.js
@@ -1,30 +1,4 @@
 window.onload = async () => {
-  // DOM catch
-  const tableBody = document.querySelector(".table_body");
-  const buttonList = document.getElementsByTagName("button");
-  const numberButtonList = document.querySelector(".number_button_list");
-  const dropdownSelect = document.querySelector(".dropdown_select");
-
-  // fetch data
-  const response = await fetch("./src/data.json");
-  const data = await response.json();
-  let dataArr = [];
-  let tempArr = [];
-  let pagePerShow = 5;
-
-  dataArr = [];
-  for (let i = 0; i < data.length; i++) {
-    tempArr.push(data[i]);
-    if (tempArr.length === pagePerShow) {
-      dataArr.push(tempArr);
-      tempArr = [];
-    }
-    if (i === data.length - 1) {
-      if (tempArr.length !== 0) dataArr.push(tempArr);
-      tempArr = [];
-    }
-  }
-
   //functions
   const showData = () => {
     for (let j = 0; j < dataArr[dataArrIndex].length; j++) {
@@ -41,26 +15,52 @@ window.onload = async () => {
     }
   };
 
+  const dataArrSetting = () => {
+    for (let i = 0; i < data.length; i++) {
+      tempArr.push(data[i]);
+      if (tempArr.length === pagePerShow) {
+        dataArr.push(tempArr);
+        tempArr = [];
+      }
+      if (i === data.length - 1) {
+        if (tempArr.length !== 0) dataArr.push(tempArr);
+        tempArr = [];
+      }
+    }
+  };
+
   const buttonColorUpdate = () => {
-    for (let j = 0; j < numberButtonList.children.length; j++) {
-      if (Number(numberButtonList.children[j].dataset.value) === dataArrIndex) {
-        numberButtonList.children[j].style.color = "red";
+    for (let i = 0; i < numberButtonList.children.length; i++) {
+      if (Number(numberButtonList.children[i].dataset.value) === dataArrIndex) {
+        numberButtonList.children[i].style.color = "red";
       } else {
-        numberButtonList.children[j].style.color = "black";
+        numberButtonList.children[i].style.color = "black";
       }
     }
   };
 
   const buttonClickEvent = () => {
     for (let i = 0; i < buttonList.length; i++) {
+      const buttonInsideText = buttonList[i].innerText;
+      switch (isNaN(Number(buttonInsideText))) {
+        case true:
+          buttonList[i].dataset.value = buttonInsideText;
+          break;
+        case false:
+          buttonList[i].dataset.value = Number(buttonList[i].innerText) - 1;
+          break;
+      }
+      if (buttonList[i].dataset.hasEvent) {
+        continue;
+      }
+      buttonList[i].dataset.hasEvent = true;
       const buttonValue = buttonList[i].dataset.value;
       buttonList[i].addEventListener("click", () => {
-        console.log(111);
         switch (buttonValue) {
-          case "prev":
+          case "<<":
             dataArrIndex = 0;
             break;
-          case "next":
+          case ">>":
             dataArrIndex = dataArr.length - 1;
             break;
           default:
@@ -75,31 +75,37 @@ window.onload = async () => {
     }
   };
 
-  // add Event
+  // DOM catch
+  const tableBody = document.querySelector(".table_body");
+  const buttonList = document.getElementsByTagName("button");
+  const numberButtonList = document.querySelector(".number_button_list");
+  const dropdownSelect = document.querySelector(".dropdown_select");
+
+  // fetch data
+  const response = await fetch("./src/data.json");
+  const data = await response.json();
+  let dataArr = [];
+  let tempArr = [];
+  let pagePerShow = 5;
   let dataArrIndex = 0;
+
+  dataArr = [];
+  dataArrSetting();
+
+  // add Event
   showData();
   buttonClickEvent();
 
   dropdownSelect.addEventListener("change", (e) => {
     pagePerShow = Number(e.target.value);
     dataArr = [];
-    for (let i = 0; i < data.length; i++) {
-      tempArr.push(data[i]);
-      if (tempArr.length === pagePerShow) {
-        dataArr.push(tempArr);
-        tempArr = [];
-      }
-      if (i === data.length - 1) {
-        if (tempArr.length !== 0) dataArr.push(tempArr);
-        tempArr = [];
-      }
-    }
+    dataArrSetting();
     // console.log(dataArr);
     numberButtonList.innerHTML = ``;
     for (let i = 0; i < dataArr.length; i++) {
       numberButtonList.innerHTML += `
-              <button data-value=${i}>${i + 1}</button>
-          `;
+        <button data-value=${i}>${i + 1}</button>
+      `;
     }
     dataArrIndex = 0;
     buttonColorUpdate();
