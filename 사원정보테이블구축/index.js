55a16f301ee98926bf8ea207c4b5cb7e20df2229
kimKyeongbeom <bibix321@naver.com>
2023-09-12 11:56:41 +0900

    사원정보테이블구축 코드리팩토링 및 defer적용

--- 사원정보테이블구축/index.js
+++ 사원정보테이블구축/index.js
@@ -1,22 +1,29 @@
 window.onload = async () => {
-  // functions
-  // let, const 호이스팅으로 인한 TDZ로 인해 함수 표현식으로 작성된 코드 최상단으로 수정
-  const showData = () => {
+  // 호이스팅으로 인한 TDZ로 인해 let, const로 선언된 변수, 함수둘 코드 최상단으로 이동
+  // fetch 및 변수
+  const data = await (await fetch("./src/data.json")).json();
+  let dataArr = [];
+  let pagePerShow = 5;
+  let dataArrIndex = 0;
+
+  // 함수들
+  const showTable = () => {
+    tableBody.innerHTML = ``;
     for (let j = 0; j < dataArr[dataArrIndex].length; j++) {
       tableBody.innerHTML += `
-              <tr style="background-color : ${
-                j % 2 === 1 ? "lightgray" : "white"
-              }">
-                  <td>${dataArr[dataArrIndex][j].name}</td>
-                  <td>${dataArr[dataArrIndex][j].title}</td>
-                  <td>${dataArr[dataArrIndex][j].email}</td>
-                  <td>${dataArr[dataArrIndex][j].role}</td>
-              </tr>
-          `;
+        <tr style="background-color : ${j % 2 === 1 ? "lightgray" : "white"}">
+            <td>${dataArr[dataArrIndex][j].name}</td>
+            <td>${dataArr[dataArrIndex][j].title}</td>
+            <td>${dataArr[dataArrIndex][j].email}</td>
+            <td>${dataArr[dataArrIndex][j].role}</td>
+        </tr>
+      `;
     }
   };
 
-  const dataArrSetting = () => {
+  const updateDataArr = () => {
+    dataArr = [];
+    let tempArr = [];
     for (let i = 0; i < data.length; i++) {
       tempArr.push(data[i]);
       if (tempArr.length === pagePerShow) {
@@ -28,6 +35,7 @@ window.onload = async () => {
         tempArr = [];
       }
     }
+    // console.log(dataArr);
   };
 
   const buttonColorUpdate = () => {
@@ -68,41 +76,30 @@ window.onload = async () => {
             dataArrIndex = Number(buttonValue);
             break;
         }
-        // console.log(dataArrIndex)
         buttonColorUpdate();
-        tableBody.innerHTML = ``;
-        showData();
+        showTable();
       });
     }
   };
 
-  // DOM catch
+  // DOM 접근
   const tableBody = document.querySelector(".table_body");
   const buttonList = document.getElementsByTagName("button");
   const numberButtonList = document.querySelector(".number_button_list");
   const dropdownSelect = document.querySelector(".dropdown_select");
 
-  // fetch data
-  const response = await fetch("./src/data.json");
-  const data = await response.json();
-  let dataArr = [];
-  let tempArr = [];
-  let pagePerShow = 5;
-  let dataArrIndex = 0;
-  dataArr = [];
-
-  // initializing
-  dataArrSetting();
-  showData();
+  // 초기세팅
+  updateDataArr();
+  showTable();
   buttonClickEvent();
 
-  // add Event
+  // 이벤트들
   dropdownSelect.addEventListener("change", (e) => {
     pagePerShow = Number(e.target.value);
-    dataArr = [];
-    dataArrSetting();
-    // console.log(dataArr);
-    numberButtonList.innerHTML = ``;
+    updateDataArr();
+    while (numberButtonList.children.length) {
+      numberButtonList.removeChild(numberButtonList.children[0]);
+    }
     for (let i = 0; i < dataArr.length; i++) {
       numberButtonList.innerHTML += `
         <button data-value=${i}>${i + 1}</button>
@@ -110,8 +107,7 @@ window.onload = async () => {
     }
     dataArrIndex = 0;
     buttonColorUpdate();
-    tableBody.innerHTML = ``;
-    showData();
+    showTable();
     buttonClickEvent();
   });
 };
